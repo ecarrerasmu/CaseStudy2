caa2250355ef7c6b1320b932141d688eed8eb2a3
ecarrerasmu <ecarreraruvalcaba@smu.edu>
2017-08-20 15:48:48 -0400

    v1

---
+++ Download.R
@@ -0,0 +1,13 @@
+#####################
+#JMJ
+#Download the data
+
+####################
+
+#Download the data
+
+#Download the data with the temperatures by Country
+Temp<-read.csv("Temp.csv", stringsAsFactors = FALSE)
+
+#Download the data with the temperatures by City
+CityTemp<-read.csv("CityTemp.csv", stringsAsFactors = FALSE)
